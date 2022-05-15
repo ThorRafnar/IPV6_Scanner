21ef0c15b1b27471b069cee97f3aef996ad20c2b
ThorRafnar <thorsteinningis@gmail.com>
2022-05-15 16:04:23 +0000

    Removed data directory

--- src/data/create_database.py
+++
@@ -1,7 +0,0 @@
-from main import Address, Port, association_table ,engine,Base
-
-######################################
-#   CREATED BY Arnjee & ThorRafnar   #
-######################################
-
-Base.metadata.create_all(engine)
